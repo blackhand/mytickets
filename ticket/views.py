7931b44d96ecad281aa005ac9c2cea6c0366e39a
yonsy <blackhand@titan.aureal.com.pe>
2009-06-10 17:21:17 -0500

    agregado registration thingies

--- ticket/views.py
+++ ticket/views.py
@@ -1,12 +1,13 @@
 # -*- coding: UTF-8 -*-
 
-from common.shortcuts import render_response
-
 from django.http import HttpResponse
 from django.views.generic.list_detail import object_list, object_detail
+from django.shortcuts import get_object_or_404
 
 from ticket.models import Event
 from ticket.constants import MAX_EVENTS_PER_PAGE
+
+from datetime import date, timedelta
 
 def homepage(request):
     return event_list(request)
@@ -37,7 +38,20 @@ def event_detail(request, event_id):
     this view manage the detail for and event
     """
     queryset = Event.objects.all()
-    extra_context = {}
+    dates = list()
+    event = get_object_or_404(Event, id=event_id)
+
+    start_date = event.start_date
+    end_date = event.end_date
+
+    while start_date <= end_date:
+        dates.append(start_date)
+        start_date += timedelta(days=1)
+
+    extra_context = {
+            'dates': dates,
+            }
+
     return object_detail(request,
             queryset = queryset,
             object_id = event_id,
